90f78944cffbb975deb75bce6a11c2efd56c9917
Mark Prado <otsukaranz@gmail.com>
2020-04-11 02:25:13 -0400

    Working tweet with latest numbers

--- twitcovidstats.py
+++ twitcovidstats.py
@@ -1,5 +1,7 @@
 import json, os
 import twitter
+from datetime import datetime
+import requests
 
 class TwitCovidStats:
     def __init__(self):
@@ -11,13 +13,26 @@ class TwitCovidStats:
                         access_token_key=auth['Access_token'],
                         access_token_secret=auth['Access_token_secret'])
 
-    def postUpdate(self,message):
-        message = "{} \r\nhttps://github.com/mrkprdo/twitcovidstats".format(message)
-        self.api.PostUpdate(message)
+    def tweetUpdate(self,message):
+        dt = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+        source = 'https://api.apify.com/v2/key-value-stores/lFItbkoNDXKeSWBBA/records/LATEST?disableRedirect=true'
+        footer = 'This is an automated tweet.'
+        message = "{}\r\nas of {}\r\nSource: {}\r\n{}\r\nhttps://github.com/mrkprdo/twitcovidstats".format(message,dt,source,footer)
+        post = self.api.PostUpdate(message)
 
+    def getLatestNumber(self):
+        respone = requests.get('https://api.apify.com/v2/key-value-stores/lFItbkoNDXKeSWBBA/records/LATEST?disableRedirect=true')
+        return respone.json()
 
 if __name__ == "__main__":
 
-    myhost = os.uname()[1]
     tt = TwitCovidStats()
-    tt.postUpdate('Test Tweet from {}'.format(myhost))
+    latestUpdate = tt.getLatestNumber()
+    message = """PH Covid19 UPDATE:
+
+    Infected: {}
+    Recovered: {}
+    Deceased: {}
+    """.format(latestUpdate['infected'],latestUpdate['recovered'],latestUpdate['deceased'])
+
+    tt.tweetUpdate(message)
